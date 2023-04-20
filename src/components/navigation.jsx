0608cbdf3babd37edc70f067f7f990cf29d797fd
Snigdh Karki <snigdhkarki@gmail.com>
2023-04-20 11:51:49 +0545

    final2

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -15,9 +15,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{' '}
             <span className='icon-bar'></span>{' '}
           </button>
-          <a className='navbar-brand page-scroll' href='#page-top'>
-            React Landing Page
-          </a>{' '}
+          
         </div>
 
         <div
